557d5b60a2cc25f0eab8da618ce92520a568922d
watway <craig@watway.net>
2021-10-11 17:51:39 +1000

    up to and including cells

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <p>
+      This site was created to demonstrate my mastery of Redwood: Look on my
+      works, ye mighty, and despair!
+    </p>
+  )
+}
+
+export default AboutPage
